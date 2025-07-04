d839915f279085252a2903155e109a57a8cda372
RHYTHAN VARSHA JANAKIRAM <152013507+Rhythanvarsha@users.noreply.github.com>
2025-07-04 08:56:39 +0530

    Add files via upload

---
+++ day2/src/test/java/com/example/SpringBotApp/SpringBotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBotApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
